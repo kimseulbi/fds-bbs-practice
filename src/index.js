cc0c7924b9ae170d0ce4ae17c2d84a4848942768
kimseulbi <ksb5166@gmail.com>
2018-11-06 18:06:44 +0900

    지금까지있던 버그 수정

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
-import "@babel/polyfill";
+import '@babel/polyfill'
 
-import axios from "axios";
+import axios from 'axios'
 
 const api = axios.create({
   // 바깥에서 주입해준 환경변수를 사용하는 코드
-  // 이 컴퓨터에서만 사용할 환경변수를 설정하기
+  // 이 컴퓨터에서만 사용할 환경변수를 설정하기 위해서 .env 파일을 편집하면 된다.
   baseURL: process.env.API_URL
-});
+})
 
-api.interceptors.request.use(function(config) {
-  const token = localStorage.getItem("token");
+api.interceptors.request.use(function (config) {
+  const token = localStorage.getItem('token')
   if (token) {
-    config.headers = config.headers || {};
-    config.headers["Authorization"] = "Bearer " + token;
+    config.headers = config.headers || {}
+    config.headers['Authorization'] = 'Bearer ' + token
   }
-  return config;
+  return config
 });
 
 const templates = {
-  loginForm: document.querySelector("#login-form").content,
-  postList: document.querySelector("#post-list").content,
-  postItem: document.querySelector("#post-item").content,
-  postForm: document.querySelector("#post-form").content,
-  postDetail: document.querySelector("#post-detail").content,
-  commentItem: document.querySelector("#comment-item").content
-};
+  loginForm: document.querySelector('#login-form').content,
+  postList: document.querySelector('#post-list').content,
+  postItem: document.querySelector('#post-item').content,
+  postForm: document.querySelector('#post-form').content,
+  postDetail: document.querySelector('#post-detail').content,
+  commentItem: document.querySelector('#comment-item').content,
+}
 
-const rootEl = document.querySelector(".root");
+const rootEl = document.querySelector('.root')
 
 // 페이지 그리는 함수 작성 순서
 // 1. 템플릿 복사
@@ -38,112 +38,113 @@ const rootEl = document.querySelector(".root");
 
 async function drawLoginForm() {
   // 1. 템플릿 복사
-  const frag = document.importNode(templates.loginForm, true);
+  const frag = document.importNode(templates.loginForm, true)
 
   // 2. 요소 선택
-  const formEl = frag.querySelector(".login-form");
+  const formEl = frag.querySelector('.login-form')
 
   // 3. 필요한 데이터 불러오기 - 필요없음
   // 4. 내용 채우기 - 필요없음
   // 5. 이벤트 리스너 등록하기
-  formEl.addEventListener("submit", async e => {
-    e.preventDefault();
-    const username = e.target.elements.username.value;
-    const password = e.target.elements.password.value;
+  formEl.addEventListener('submit', async e => {
+    e.preventDefault()
+    const username = e.target.elements.username.value
+    const password = e.target.elements.password.value
 
-    const res = await api.post("/users/login", {
+    const res = await api.post('/users/login', {
       username,
       password
-    });
+    })
 
-    localStorage.setItem("token", res.data.token);
-    drawPostList();
-  });
+    localStorage.setItem('token', res.data.token)
+    drawPostList()
+  })
 
   // 6. 템플릿을 문서에 삽입
-  rootEl.textContent = "";
-  rootEl.appendChild(frag);
+  rootEl.textContent = ''
+  rootEl.appendChild(frag)
 }
 
 async function drawPostList() {
   // 1. 템플릿 복사
-  const frag = document.importNode(templates.postList, true);
+  const frag = document.importNode(templates.postList, true)
 
   // 2. 요소 선택
-  const listEl = frag.querySelector(".post-list");
+  const listEl = frag.querySelector('.post-list')
   const createEl = frag.querySelector('.create')
 
   // 3. 필요한 데이터 불러오기
-  // data 속성에 postList에 저장
-  // const res  = await api.get("/posts?_embad=user");
+  const { data: postList } = await api.get('/posts?_expand=user')
+  // const res = await api.get('/posts?_embed=user')
   // const postList = res.data
-  const { data: postList } = await api.get("/posts?_expand=user");
 
   // 4. 내용 채우기
   for (const postItem of postList) {
-    const frag = document.importNode(templates.postItem, true);
+    const frag = document.importNode(templates.postItem, true)
 
-    const idEl = frag.querySelector(".id");
-    const titleEl = frag.querySelector(".title");
-    const authorEl = frag.querySelector(".author");
+    const idEl = frag.querySelector('.id')
+    const titleEl = frag.querySelector('.title')
+    const authorEl = frag.querySelector('.author')
 
-    idEl.textContent = postItem.id;
-    titleEl.textContent = postItem.title;
-    authorEl.textContent = postItem.user.username;
+    idEl.textContent = postItem.id
+    titleEl.textContent = postItem.title
+    authorEl.textContent = postItem.user.username
 
-    titleEl.addEventListener("click", e => {
-      drawPostDetail(postItem.id);
-    });
+    titleEl.addEventListener('click', e => {
+      drawPostDetail(postItem.id)
+    })
 
-    listEl.appendChild(frag);
+    listEl.appendChild(frag)
   }
 
   // 5. 이벤트 리스너 등록하기
-  createEl.addEventListener('click',e => {
+  createEl.addEventListener('click', e => {
     drawNewPostForm()
   })
 
   // 6. 템플릿을 문서에 삽입
-  rootEl.textContent = "";
-  rootEl.appendChild(frag);
+  rootEl.textContent = ''
+  rootEl.appendChild(frag)
 }
 
 async function drawPostDetail(postId) {
   // 1. 템플릿 복사
-  const frag = document.importNode(templates.postDetail, true);
+  const frag = document.importNode(templates.postDetail, true)
 
   // 2. 요소 선택
-  const titleEl = frag.querySelector(".title");
-  const authorEl = frag.querySelector(".author");
-  const bodyEl = frag.querySelector(".body");
-  const backEl = frag.querySelector(".back");
-  const commentListEl = frag.querySelector(".comment-list");
-  const commentFormEl = frag.querySelector(".comment-form")
-  const updateEl = frag.querySelector(".update");
+  const titleEl = frag.querySelector('.title')
+  const authorEl = frag.querySelector('.author')
+  const bodyEl = frag.querySelector('.body')
+  const backEl = frag.querySelector('.back')
+  const commentListEl = frag.querySelector('.comment-list')
+  const commentFormEl = frag.querySelector('.comment-form')
+  const updateEl = frag.querySelector('.update')
 
   // 3. 필요한 데이터 불러오기
-  const {
-    data: { title, body, user, comments }
-  } = await api.get("/posts/" + postId, {
+  const { data: { title, body, user, comments } } = await api.get('/posts/' + postId, {
     params: {
-      _expand: "user",
-      _embad: "comments"
+      _expand: 'user',
+      _embed: 'comments'
     }
-  });
-  const params = new URLSearchParams()
-  comments.forEach( c => {
-    params.append('id',c.userId)
   })
-
-  const {data: userList} = await api.get('/users',{
+  const params = new URLSearchParams()
+  comments.forEach(c => {
+    params.append('id', c.userId)
+  })
+  // const res = await api.get('/users', {
+  //   params
+  // })
+  // const userList = res.data
+  const { data: userList } = await api.get('/users', {
     params
   })
 
   // 4. 내용 채우기
-  titleEl.textContent = title;
-  bodyEl.textContent = body;
-  authorEl.textContent = user.username;
+  titleEl.textContent = title
+  bodyEl.textContent = body
+  authorEl.textContent = user.username
 
+  console.log(comments)
   // 댓글 표시
   for (const commentItem of comments) {
     // 1. 템플릿 복사
@@ -154,51 +155,54 @@ async function drawPostDetail(postId) {
     const bodyEl = frag.querySelector('.body')
     const deleteEl = frag.querySelector('.delete')
 
-    // 3. 필요한 데이터 불러오기
-
+    // 3. 필요한 데이터 불러오기 - 필요없음
     // 4. 내용 채우기
     bodyEl.textContent = commentItem.body
+
     const user = userList.find(item => item.id === commentItem.userId)
     authorEl.textContent = user.username
 
     // 5. 이벤트 리스너 등록하기
-    commentFormEl.addEventListener('submit', async e => {
-      e.preventDefault()
-      const body = e.target.elements.body.value
-      await api.post(`/post/${postId}/comments`, {
-        body
-      })
-      drawPostDetail(postId)
-    })
     // 6. 템플릿을 문서에 삽입
     commentListEl.appendChild(frag)
   }
 
   // 5. 이벤트 리스너 등록하기
-  backEl.addEventListener("click", e => {
-    drawPostList();
-  });
+  backEl.addEventListener('click', e => {
+    drawPostList()
+  })
 
   updateEl.addEventListener('click', e => {
     drawEditPostForm(postId)
   })
 
+  commentFormEl.addEventListener('submit', async e => {
+    e.preventDefault()
+    const body = e.target.elements.body.value
+    await api.post(`/posts/${postId}/comments`, {
+      body
+    })
+    drawPostDetail(postId)
+  })
+
   // 6. 템플릿을 문서에 삽입
-  rootEl.textContent = ""; // 다시 다 지우고 다시 그려라
-  rootEl.appendChild(frag);
+  rootEl.textContent = ''
+  rootEl.appendChild(frag)
 }
 
 async function drawNewPostForm() {
   // 1. 템플릿 복사
   const frag = document.importNode(templates.postForm, true)
+
   // 2. 요소 선택
   const formEl = frag.querySelector('.post-form')
-  const backEl = frag.querySelector(".back");
+  const backEl = frag.querySelector('.back')
+
   // 3. 필요한 데이터 불러오기
   // 4. 내용 채우기
   // 5. 이벤트 리스너 등록하기
-  formEl.addEventListener('submit',async e => {
-    e.preventDefault();
+  formEl.addEventListener('submit', async e => {
+    e.preventDefault()
     const title = e.target.elements.title.value
     const body = e.target.elements.body.value
     await api.post('/posts', {
@@ -211,6 +215,7 @@ async function drawNewPostForm() {
     e.preventDefault()
     drawPostList()
   })
+
   // 6. 템플릿을 문서에 삽입
   rootEl.textContent = ''
   rootEl.appendChild(frag)
@@ -222,12 +227,12 @@ async function drawEditPostForm(postId) {
 
   // 2. 요소 선택
   const formEl = frag.querySelector('.post-form')
-  const backEl = frag.querySelector(".back");
+  const backEl = frag.querySelector('.back')
   const titleEl = frag.querySelector('.title')
   const bodyEl = frag.querySelector('.body')
 
   // 3. 필요한 데이터 불러오기
-  const {data: {title, body}} = await api.get('/posts/' +postId)
+  const { data: { title, body } } = await api.get('/posts/' + postId)
 
   // 4. 내용 채우기
   titleEl.value = title
@@ -235,7 +240,7 @@ async function drawEditPostForm(postId) {
 
   // 5. 이벤트 리스너 등록하기
   formEl.addEventListener('submit', async e => {
-    e.preventDefault();
+    e.preventDefault()
     const title = e.target.elements.title.value
     const body = e.target.elements.body.value
     await api.patch('/posts/' + postId, {
@@ -255,8 +260,8 @@ async function drawEditPostForm(postId) {
 }
 
 // 페이지 로드 시 그릴 화면 설정
-if (localStorage.getItem("token")) {
-  drawPostList();
+if (localStorage.getItem('token')) {
+  drawPostList()
 } else {
-  drawLoginForm();
+  drawLoginForm()
 }
